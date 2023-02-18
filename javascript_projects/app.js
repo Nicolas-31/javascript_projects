1a67ef6cc083f99a0197f0198d06b2fdb1e73fdc
Nicolas Anderson <nicolas-31@outlook.com>
2023-02-18 20:59:16 +0100

    updated

---
+++ javascript_projects/app.js
@@ -0,0 +1,22 @@
+
+// variable - Most Basic Building Block
+// variables - Store, Access, Modify === Value
+// Declare, Assignment Operator, Assign Value
+// assign value later, modify existing
+
+// let name = 'John';
+// let address, zip, state;
+// address = '123 Main St';
+// zip = '12345';
+// state = 'CA';
+// console.log(address, zip, state);
+// console.log(name);
+
+// can contain digits, letters, underscore, $
+// must start with letter, $ or _
+// no keyword
+// cannot start with number
+
+// case sensitive - fullname vs fullname 
+// let random123_$ = 'random';
+// console.log(random123_$);
---
+++ javascript_projects/challenges/01.js
@@ -0,0 +1,5 @@
+const firstName = 'John';
+const lastName = 'Doe';
+let address = 'main street';
+address = 'first street';
+console.log(firstName, lastName, address);
